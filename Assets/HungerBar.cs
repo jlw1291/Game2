d645d0821df220e2b636da73ca958551c9d2975a
haleyhagler <haleyhagler@gmail.com>
2021-03-08 16:58:21 -0600

    Robert's files
    unzipped the file Robert pushed

---
+++ Assets/HungerBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HungerBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetMaxHunger(float hunger)
+    {
+        slider.maxValue = hunger;
+        slider.value = hunger;
+    }
+
+    public void SetHunger(float hunger)
+    {
+        slider.value = hunger;
+    }
+}
+
