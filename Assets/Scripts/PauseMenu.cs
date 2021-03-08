4d6c057f6002f5eb295d07cac4843446a86fc8fc
haleyhagler <haleyhagler@gmail.com>
2021-03-08 16:47:48 -0600

    Connors Levels
    Connors levels added to the project

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+    public GameObject pausemenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused) { Resume(); }
+            else { Pause(); }
+        }
+    }
+
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Reasource.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InventoryReasource;
+
+public class Reasource : Enemy
+{
+
+	public GameObject player;
+
+    public override void TakeDamage(int damage){
+    	//new InventoryReasource(gameObject.name);
+    	player.GetComponent<Inventory>().addItem( new InventoryReasourceClass(gameObject.name));
+		
+		Destroy (gameObject);			
+	}
+}
