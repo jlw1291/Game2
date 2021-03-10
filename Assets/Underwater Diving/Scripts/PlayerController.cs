c28b84d5d58184f8cac0af289aacc5ae4548284e
RobertMcGee <mcgeerobert967@gmail.com>
2021-03-10 01:47:06 -0600

    Pause Menu, Crafting Menu
    I implemented connor's pause menu scene and added a menu for the crafting as well as added sounds here and there. I used a different inventory file called RobInvScript just to get the basics down and so its able to merge well with Haley's Inv system.

--- Assets/Underwater Diving/Scripts/PlayerController.cs
+++ Assets/Underwater Diving/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:Assets/Underwater Diving/Scripts/PlayerController.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -86,3 +87,87 @@ public class PlayerController : MonoBehaviour{
 
 
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+	public float moveSpeed;
+	public bool rushing = false;
+	private float speedMod = 0;
+
+	float timeLeft = 2f;
+
+	public float imageScale;
+
+	private Rigidbody2D myRigidBody;
+
+	private Animator myAnim;
+
+
+
+	// Use this for initialization
+	void Start (){
+		myRigidBody = GetComponent<Rigidbody2D> ();	
+		myAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+		resetBoostTime ();
+		controllerManager ();
+
+
+
+		//myAnim.SetFloat ("Speed", Mathf.Abs(myRigidBody.velocity.x));
+
+	 
+		
+	}
+
+	void controllerManager (){
+		if (Input.GetAxisRaw ("Horizontal") > 0f) {
+			transform.localScale = (new Vector3(1f,1f,1f))*imageScale;
+			movePlayer ();
+		} else if (Input.GetAxisRaw ("Horizontal") < 0f) {			
+			transform.localScale = (new Vector3(-1f,1f,1f)) * imageScale;
+			movePlayer ();
+		} else if (Input.GetAxisRaw ("Vertical") > 0f) {
+			myRigidBody.velocity = new Vector3 (myRigidBody.velocity.x, moveSpeed, 0f);
+		} else if (Input.GetAxisRaw ("Vertical") < 0f) {
+			myRigidBody.velocity = new Vector3 (myRigidBody.velocity.x, -moveSpeed, 0f);
+		}
+
+		if(Input.GetButtonDown("Jump") && !rushing ){
+			rushing = true;
+			speedMod = 2;
+			//Instantiate (bubbles, gameObject.transform.position, gameObject.transform.rotation);
+			movePlayer ();
+		}	
+	}
+
+	void movePlayer(){
+		if (transform.localScale.x == 1*imageScale) {
+			myRigidBody.velocity = new Vector3 (moveSpeed + speedMod, myRigidBody.velocity.y, 0f);	
+		} else {
+			myRigidBody.velocity = new Vector3 (- (moveSpeed + speedMod), myRigidBody.velocity.y, 0f);
+		}	
+	}
+
+	void resetBoostTime(){
+		if (timeLeft <= 0) {
+			timeLeft = 2f;
+			rushing = false;
+			speedMod = 0;
+		} else if(rushing) {
+			timeLeft -= Time.deltaTime;
+		}	
+	}
+
+
+
+}
+>>>>>>> Stashed changes:Assets/Scripts/Player/PlayerController.cs
