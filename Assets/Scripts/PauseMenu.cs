4b3b5644647c0f0951afc5c7aa454848dd5167e6
RobertMcGee <mcgeerobert967@gmail.com>
2021-03-09 22:52:05 -0600

    Add files via upload

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject PauseMenuUI;
+    public GameObject CraftMenuUI;
+    public GameObject StartMenuUI;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+        PauseMenuUI.SetActive(false);
+        CraftMenuUI.SetActive(false);
+        StartMenuUI.SetActive(false);
+    }
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+                
+            }
+        }
+      if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            //Crafting Menu
+        }
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        Debug.Log("Pausing game");
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        
+        Time.timeScale = 1f;
+        GamePaused = false;
+        Debug.Log("Resuming Game");
+    }
+
+    public void LoadMenu()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game!");
+        Application.Quit();
+    }
+}
