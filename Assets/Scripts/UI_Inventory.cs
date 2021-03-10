4a2cbe737af4e61015a186cc8a80f516e0c1ed81
haleyhagler <haleyhagler@gmail.com>
2021-03-10 15:35:39 -0600

    This is a buggy inventory

--- Assets/Scripts/UI_Inventory.cs
+++ Assets/Scripts/UI_Inventory.cs
@@ -8,41 +8,21 @@ public class UI_Inventory : MonoBehaviour
 {
     private Inventory inventory;
 
-    /*private Transform inventoryHolder;
-    private Transform inventoryHolderTemplate;
-    public GameObject template;*/
-    //private Transform itemSlotContainer;
-    private Transform itemSlotSuit;
-    private Transform itemSlotMelee;
-    private Transform itemSlotRange;
-    private Transform itemSlotLamp;
-    private Transform itemSlotFins;
+    private Transform itemSlotContainer;
 
-    private Transform itemSlotSeaweed;
-    private Transform itemSlotFishcan;
-    private Transform itemSlotMetal; 
-    private Transform itemSlotWood;
-    private Transform itemSlotBolt;   
+    private Transform gearSlotTemp;
+    private Transform itemSlotTemplate;
 
 
     private void Awake(){
 
-        /*inventoryHolder = transform.Find("UI_Inventory_Container");
-        inventoryHolderTemplate = inventoryHolder.transform.Find("UI_Inventory_Template");
-        inventoryHolderTemplate.SetActive(false);*/
+        itemSlotContainer = transform.Find("InventoryUI");
+        print("found inventory");
 
-    	itemSlotSuit = transform.Find("itemSlotSuit");
-        itemSlotMelee = transform.Find("itemSlotMelee");
-        itemSlotRange = transform.Find("itemSlotRange");
-        itemSlotLamp = transform.Find("itemSlotLamp");
-        itemSlotFins = transform.Find("itemSlotFins");
+        
+        itemSlotTemplate = transform.Find("ResourceSlotTemp");
 
-
-        itemSlotSeaweed = transform.Find("itemSlotSeaweed");
-        itemSlotFishcan = transform.Find("itemSlotFishcan");
-        itemSlotMetal = transform.Find("itemSlotMetal");
-        itemSlotWood = transform.Find("itemSlotWood");
-        itemSlotBolt = transform.Find("itemSlotBolt");
+        print("found temp");
         
     }
 
@@ -60,108 +40,112 @@ public class UI_Inventory : MonoBehaviour
 
     public void RefreshInventoryItems(){
 
-        /*foreach (Transform child in itemSlotContainer){
-            if(child == inventoryHolderTemplate){
-                continue;
-            }
+        /*foreach (Transform child in itemSlotContainer) {
+            if (child == itemSlotTemplate) continue;
             Destroy(child.gameObject);
         }*/
-        print("Refresh Inventory");
 
+        print("Hi");
 
-    	
+        int x = 0;
+        int y = 0;
+        int width = 70;
+        int hight = 50;
 
-        ///RectTransform inventoryContainer = Instantiate( inventoryHolderTemplate,  inventoryHolderTemplate).GetComponent<RectTransform>();
-       // inventoryContainer.SetActive(true);
-
-    	//May need to destroy objects before refreshing
-
-    // Set Suit Slot
-        ResouceItemType suit = inventory.GetSuit();
-
-        //GameObject newReasouce = Instantiate (reasource, gameObject.transform.position, gameObject.transform.rotation);
-
-        Image suitimage = itemSlotSuit.Find("Image").GetComponent<Image>();
-    	suitimage.sprite = inventory.GetSprite(suit);
-		
-    // Set Fins Slot
-    	bool fins = inventory.GetFins();
-        Image finimage = itemSlotFins.Find("Image").GetComponent<Image>();
-        if(fins){ finimage.sprite = inventory.GetSprite(ResouceItemType.Fins); }
-        else{ finimage.sprite = inventory.GetSprite(ResouceItemType.None); }
-    
-    // Set Lamp Slot	
-    	bool lamp = inventory.GetLamp();
-        Image lampimage = itemSlotLamp.Find("Image").GetComponent<Image>();
-        if(lamp){ lampimage.sprite = inventory.GetSprite(ResouceItemType.Lamp); }
-        else{ lampimage.sprite = inventory.GetSprite(ResouceItemType.None); }
-
-    // Set Melee Slot
-        ResouceItemType melee = inventory.GetMelee();
-        Image meleeimage = itemSlotMelee.Find("Image").GetComponent<Image>();
-        meleeimage.sprite = inventory.GetSprite(melee);
-
-    // Set Range Slot 
-        bool range = inventory.GetRange();
-        Image rangeimage = itemSlotRange.Find("Image").GetComponent<Image>();
-        if(range){ rangeimage.sprite = inventory.GetSprite(ResouceItemType.Harpoon); }
-        else{ rangeimage.sprite = inventory.GetSprite(ResouceItemType.None); }
-
-
-
-/*Seaweed,	Fishcan,	Wood,	Metal, 	Bolt,	Spear,	Knife, 	Trident,	Harpoon,	RadSuit,	HeatSuit,	Fins, 	Lamp*/
-
-    	List<ResourceItem> list = inventory.GetItemList();
-        string l ="Current Inventory:";
-        foreach(ResourceItem item in inventory.GetItemList()){
-            l += " " + item.itemType + "-" + item.amount;
+        if(inventory.GetSuit() != ResouceItemType.None){
+            MakeSlotGear(itemSlotTemplate, inventory.GetSuit(), x, y, width, hight, 1);
+        }else{
+            MakeSlotGear(itemSlotTemplate, inventory.GetSuit(), x, y, width, hight, 0);
         }
-        print(l);
 
-    	foreach(ResourceItem item in inventory.GetItemList()){
-            print("Foreach, inventory");
-            ResouceItemType r;
-            Image image;
-            Text text;
-            
-    		switch(item.itemType){
-                case ResouceItemType.Seaweed:
-                    image = itemSlotSeaweed.Find("Image").GetComponent<Image>();
-                    text = itemSlotSeaweed.Find("Text").GetComponent<Text>();
-                    r = ResouceItemType.Seaweed;
-                    break;
-                case ResouceItemType.Fishcan:
-                    image = itemSlotFishcan.Find("Image").GetComponent<Image>();
-                    text = itemSlotFishcan.Find("Text").GetComponent<Text>();
-                    r = ResouceItemType.Fishcan;
-                    break;
-                case ResouceItemType.Metal:
-                    image = itemSlotMetal.Find("Image").GetComponent<Image>();
-                    text = itemSlotMetal.Find("Text").GetComponent<Text>();
-                    r = ResouceItemType.Metal;
-                    break;
-                case ResouceItemType.Wood:
-                    image = itemSlotWood.Find("Image").GetComponent<Image>();
-                    text = itemSlotWood.Find("Text").GetComponent<Text>();
-                    r = ResouceItemType.Wood;
-                    break;
-                case ResouceItemType.Bolt:
-                    image = itemSlotBolt.Find("Image").GetComponent<Image>();
-                    text = itemSlotBolt.Find("Text").GetComponent<Text>();
-                    r = ResouceItemType.Bolt;
-                    break;
-                default:
-                    r = ResouceItemType.None;
-                    image = null;
-                    text = null;
-                    break;
+ print("Hi");
+        x++;
+
+        if(inventory.GetRange()){
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.Harpoon, x, y, width, hight, 1);
+        }else{
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.None, x, y, width, hight, 0);
+        }
+
+ print("Hi");
+        x++;
+
+        if(inventory.GetFins()){
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.Fins, x, y, width, hight, 1);
+        }else{
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.None, x, y, width, hight, 0);
+        }
+
+ print("Hi");
+        x++;
+
+        if(inventory.GetLamp()){
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.Lamp, x, y, width, hight, 1);
+        }else{
+            MakeSlotGear(itemSlotTemplate, ResouceItemType.None, x, y, width, hight, 0);
+        }
+
+
+        x++;
+
+
+        if(inventory.GetMelee() != ResouceItemType.None){
+            MakeSlotGear(itemSlotTemplate, inventory.GetMelee(), x, y, width, hight, 1);
+        }else{
+            MakeSlotGear(itemSlotTemplate, inventory.GetMelee(), x, y, width, hight, 0);
+        }
+
+        x++;
+
+        foreach (ResourceItem item in inventory.GetItemList()) {
+           MakeSlot(itemSlotTemplate, item, x, y, width, hight);
+
+            x++;
+            if (x >= 5) {
+                x = 0;
+                y++;
             }
+        }
+    }
 
-            if(item.amount > 0){ image.sprite = inventory.GetSprite(r); }
-            else{ image.sprite = inventory.GetSprite(ResouceItemType.None); }
-            text.text = item.amount.ToString();
-            print("item: " + item.itemType + " amount: " + item.amount);
-    	}
+    private void MakeSlot(Transform itemSlotTemplate, ResourceItem item, int x, int y, int width, int hight){
+
+        RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+        itemSlotRectTransform.gameObject.SetActive(true);
+        
+        /*itemSlotRectTransform.GetComponent<Button_UI>().ClickFunc = () => {
+            // Use item
+            inventory.UseItem(item);
+        };*/
+        
+        itemSlotRectTransform.anchoredPosition = new Vector2(x * width, -y * hight);
+        Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+        Text text = itemSlotRectTransform.Find("Text").GetComponent<Text>();
+
+        if(item.amount > 0){ image.sprite = inventory.GetSprite(item.itemType); }
+        else{ image.sprite = inventory.GetSprite(ResouceItemType.None); }
+        
+        text.text = item.amount.ToString();
+    }
+
+    private void MakeSlotGear(Transform itemSlotTemplate, ResouceItemType item, int x, int y, int width, int hight, int exists){
+
+        RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+        itemSlotRectTransform.gameObject.SetActive(true);
+        
+        /*itemSlotRectTransform.GetComponent<Button_UI>().ClickFunc = () => {
+            // Use item
+            inventory.UseItem(item);
+        };*/
+        
+        itemSlotRectTransform.anchoredPosition = new Vector2(x * width, -y * hight);
+        Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+        Text text = itemSlotRectTransform.Find("Text").GetComponent<Text>();
+
+        if(exists > 0){ image.sprite = inventory.GetSprite(item); }
+        else{ image.sprite = inventory.GetSprite(ResouceItemType.None); }
+        
+        text.text = "1";
     }
 
 }
